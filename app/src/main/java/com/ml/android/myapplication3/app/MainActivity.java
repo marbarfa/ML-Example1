0f3c1d8e16cadd692ef959df070afb0da3693270
mbarreto <marbarfa@gmail.com>
2014-04-13 20:44:37 -0300

    Initial commit

---
+++ app/src/main/java/com/ml/android/myapplication3/app/MainActivity.java
@@ -0,0 +1,71 @@
+package com.ml.android.myapplication3.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity implements android.view.View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button1).setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(android.view.View view) {
+        final android.app.ProgressDialog dialog = new android.app.ProgressDialog(getApplicationContext());
+        dialog.setMessage("Buscando: "+((android.widget.EditText)findViewById(R.id.input)).getText().toString());
+        dialog.show();
+
+        android.os.AsyncTask asyncTask = new android.os.AsyncTask<Void, Void, Object>() {
+
+            @Override
+            protected Object doInBackground(Void... voids) {
+                //EXECUTE SOMETHING.
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                super.onPostExecute(o);
+                if (dialog.isShowing())
+                    dialog.dismiss();
+                //UPDATE UI
+            }
+
+
+        };
+
+        asyncTask.execute();
+    }
+}

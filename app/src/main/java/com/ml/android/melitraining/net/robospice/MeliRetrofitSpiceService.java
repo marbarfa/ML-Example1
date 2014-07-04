e550abd377241bfa9ac6df1a4cb48b88ed33fb77
Marcos Barreto <marcos.barreto@mercadolibre.com>
2014-07-04 15:12:19 -0300

    Bookmarks service  Add a bookmark to an item.  Use a db.sqlite to save the bookmarks  Use ormlite to retrieve/save/update bookmarks in the DB.  DB: Helpers and DAO.  Fix problem while retrieving items from network.

--- app/src/main/java/com/ml/android/melitraining/net/robospice/MeliRetrofitSpiceService.java
+++ app/src/main/java/com/ml/android/melitraining/net/robospice/MeliRetrofitSpiceService.java
@@ -1,17 +1,6 @@
 package com.ml.android.melitraining.net.robospice;
 
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
 import com.ml.android.melitraining.net.MeliAPI;
-
-import java.lang.reflect.Type;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Locale;
 
 public class MeliRetrofitSpiceService extends RetrofitJacksonSpiceService {
     private final static String BASE_URL = "https://api.mercadolibre.com/";
@@ -23,39 +12,9 @@ public class MeliRetrofitSpiceService extends RetrofitJacksonSpiceService {
         addRetrofitInterface(MeliAPI.SearchAPI.class);
     }
 
-//    @Override
-//    protected Converter createConverter() {
-//        Gson gson = new GsonBuilder()
-//                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-//                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
-//                .registerTypeAdapter(Date.class, new DateDeserializer())
-//                .create();
-//        return new GsonConverter(gson);
-//    }
-
     @Override
     protected String getServerUrl() {
         return BASE_URL;
     }
 
-
-    private static final String[] DATE_FORMATS = new String[]{
-            "yyyy-MM-dd'T'HH:mm:ssZ",
-            "yyyy-MM-dd"
-    };
-
-    private static class DateDeserializer implements JsonDeserializer<Date> {
-        @Override
-        public Date deserialize(JsonElement jsonElement, Type typeOF,
-                                JsonDeserializationContext context) throws JsonParseException {
-            for (String format : DATE_FORMATS) {
-                try {
-                    return new SimpleDateFormat(format, Locale.US).parse(jsonElement.getAsString());
-                } catch (ParseException e) {
-                }
-            }
-            throw new JsonParseException("Unparseable date: \"" + jsonElement.getAsString()
-                    + "\". Supported formats: " + Arrays.toString(DATE_FORMATS));
-        }
-    }
 }
